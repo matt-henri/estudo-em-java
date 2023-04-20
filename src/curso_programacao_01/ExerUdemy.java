7014b3a4e5be89475c5fd7a5016c4982d1e30410
matt-henri <mathmarkub@gmail.com>
2023-04-20 17:06:33 -0300

    correção

--- src/curso_programacao_01/ExerUdemy.java
+++ src/curso_programacao_01/ExerUdemy.java
@@ -7,22 +7,22 @@ public class ExerUdemy {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		
-		System.out.println("informe a largura do terreno: ");
+		System.out.println("Informe a largura do terreno: ");
 		int largura = entrada.nextInt();
 		
-		System.out.println("informe o comprimento do terreno: ");
+		System.out.println("Informe o comprimento do terreno: ");
 		int comprimento = entrada.nextInt();
 		
-		System.out.println("informe o valor do metro quadrado: ");
+		System.out.println("Informe o valor do metro quadrado: ");
 		int valorMetroQuadrado = entrada.nextInt();
 		
 		
 		int area = (largura * comprimento);
-		System.out.println("A area desse terreno pe de: " + area);
+		System.out.println("A area desse terreno é = " + area);
 		
 		
 		int preco = (area * valorMetroQuadrado);
-		System.out.println("O preço do terreno é de: " + preco);
+		System.out.println("O preço do terreno é = " + preco);
 
 		entrada.close();
 	}
