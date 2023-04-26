c501770a2a2f01754bc908073ba5d5c62ee6d5d1
matt-henri <mathmarkub@gmail.com>
2023-04-26 14:52:29 -0300

    exer-switch-case

--- src/curso_programacao_01/SwitchCase2.java
+++ src/curso_programacao_01/SwitchCase2.java
@@ -2,37 +2,46 @@ package curso_programacao_01;
 
 import java.util.Scanner;
 
-public class SintaxeSwitchCase {
+public class SwitchCase2 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int x = sc.nextInt();
 		String dia;
 		
-		if (x == 1) {
+		switch (x) {
+		case 1:
 			dia = "Domingo";
-		} else if (x == 2) {
+			break;
+		case 2:
 			dia = "Segunda";
-		} else if (x == 3) {
+			break;
+		case 3:
 			dia = "Terça";
-		} else if (x == 4) {
+			break;
+		case 4:
 			dia = "Quarta";
-		} else if (x == 5) {
+			break;
+		case 5:
 			dia = "Quinta";
-		} else if (x == 6) {
+			break;
+		case 6:
 			dia = "Sexta";
-		} else if (x == 7) {
+			break;
+		case 7:
 			dia = "Sábado";
-		} else {
+			break;
+		default:
 			dia = "valor inválido";
+			break;
 		}
 		
-		System.out.println("O dia da semana é " + dia);
 		
-		
+		System.out.println("Dia da Semana: " + dia);
+			
 		sc.close();
-		
 
 	}
 
