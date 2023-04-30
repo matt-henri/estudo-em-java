6c063360f0ecf112d9c7e71b7fd55f48545f5a43
matt-henri <mathmarkub@gmail.com>
2023-04-30 15:56:47 -0300

    exer-while

---
+++ src/curso_programacao_01/WhileDois.java
@@ -0,0 +1,41 @@
+package curso_programacao_01;
+
+import java.util.Scanner;
+
+public class WhileDois {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		
+		while (x != 0 && y!= 0) {
+			if (x > 0 && y > 0) {
+				System.out.println("Primeiro");
+			}
+			else if (x < 0 && y > 0) {
+				System.out.println("Segundo");
+			}
+			else if (x < 0 && y < 0) {
+				System.out.println("Terceiro");
+			}
+			else {
+				System.out.println("Quarto");
+			}
+			
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
