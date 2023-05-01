cb305191f64fdde46bfda766cb28d1a6ef60fc0d
matt-henri <mathmarkub@gmail.com>
2023-05-01 10:00:34 -0300

    for-soma

---
+++ src/curso_programacao_01/SomaFOR.java
@@ -0,0 +1,27 @@
+package curso_programacao_01;
+
+import java.util.Scanner;
+
+public class SomaFOR {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int soma = 0;
+		for ( int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			soma = soma + x;
+		}
+		
+		System.out.println(soma);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
