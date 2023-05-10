dab641880b2ac10168909e50330207d07c42a3f1
matt-henri <mathmarkub@gmail.com>
2023-05-10 17:22:01 -0300

    atualizado

--- src/curso_programacao_01/TESTES.java
+++ src/curso_programacao_01/TESTES.java
@@ -1,27 +1,11 @@
 package curso_programacao_01;
 
-import java.util.Scanner;
 
 public class TESTES {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("digite algo: ");
-		
-		String entrada = sc.nextLine();
-		String s01 = entrada.toLowerCase();
-		String s02 = entrada.toUpperCase();
-		String s03 = entrada.substring(2, 9);
-		
-		System.out.println("toLowerCase:  " + s01);
-		System.out.println("toUpperCase:  " + s02);
-		System.out.println("Substring:  " + s03);
-		
-		
-		
-		sc.close();
 		
 
 	}
