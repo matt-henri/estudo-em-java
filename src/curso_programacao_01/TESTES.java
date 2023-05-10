889e42d5653940dc5168270be1a09bc64bd500bd
matt-henri <mathmarkub@gmail.com>
2023-05-10 18:06:13 -0300

    att

--- src/curso_programacao_01/TESTES.java
+++ src/curso_programacao_01/TESTES.java
@@ -1,10 +1,27 @@
 package curso_programacao_01;
 
+import java.util.Scanner;
 
 public class TESTES {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter three numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if (a > b && a > c) {
+			System.out.println("O maior número é o " + a);
+		} else if (b > c) {
+			System.out.println("O maior número é o " + b);
+		} else {
+			System.out.println("O maior número é o " + c);
+		}
+		
+		sc.close();
 		
 		
 
